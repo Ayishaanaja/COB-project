cf85c52ab2bdf2a6542f034729d0e868a33c7009
Ayishaanaja <najaayisha77@gmail.com>
2025-12-08 17:12:25 +0530

    package

--- src/main/java/com/example/demo/bean/Patient.java
+++ src/main/java/com/example/demo/bean/Patient.java
@@ -1,5 +1,34 @@
 package com.example.demo.bean;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
 public class Patient {
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long patientId;
+
+	    @Column(nullable = false)
+	    private String name;
+
+	    @Column(nullable = false)
+	    private LocalDate dob;
+
+	    @Column(length = 10)
+	    private String gender;
+
+	    @Column
+	    private String contactInfo;
+
+	    @Column(nullable = false)
+	    private Double outOfPocketMax;
+
+	    @Column(nullable = false)
+	    private Double currentOutOfPocketSpent;
 
 }
